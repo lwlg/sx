fb76a2786568df6e2dc5e8e7026293b206e89898
lwlg <lwlgzy@126.com>
2018-12-27 17:20:50 +0800

    增加商品入库界面

---
+++ glsys/sxpos/Manage/GoodIn.cs
@@ -0,0 +1,24 @@
+﻿/*
+    商品入库
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NDolls.Core;
+
+
+namespace sxpos.Manage
+{
+    public partial class GoodIn : BaseForm
+    {
+        public GoodIn()
+        {
+            InitializeComponent();
+        }
+    }
+}
