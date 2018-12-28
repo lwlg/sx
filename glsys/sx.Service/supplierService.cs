0cdedcc6cc28710261402afbde180340e4ec9d3c
lwlg <lwlgzy@126.com>
2018-12-28 10:36:36 +0800

    增加供应商服务

---
+++ glsys/sx.Service/supplierService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NDolls.Core;
+using NDolls.Data;
+using NDolls.Data.Entity;
+
+namespace sx.Service
+{
+   public class SupplierService:ServiceBase<sx.Model.pos_Supplier>
+    {
+       
+        public List<sx.Model.pos_Supplier> GetModels(string keywords)
+        {
+          keywords = NDolls.Core.Util.ValidateUtil.FilterIllegal(keywords);
+          return r.Find("SName LIKE '%"+keywords+"%' OR ShortCode LIKE '%"+keywords+"%'");
+        }
+    }
+}
